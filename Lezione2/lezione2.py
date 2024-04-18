47b6aa3f368e5937fda44693d60e48e2d46c68ca
Gajiros <gabbi.jimros@gmail.com>
2024-04-18 10:00:39 +0200

    Upload 2 esercizi lezione 2

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -7,8 +7,49 @@
 to represent a person’s name, and print a 
 message to that person. Your message should 
 be simple, such as, “Hello Eric, would you 
-like to learn some Python today?”'''
+like to learn some Python today?”
 
 name: str = 'Mario'
 message: str = f'Ciao {name}, ti va di imparare un po\' di python insieme'
 print(message)
+
+# 2-4
+name1: str = 'Giulio'
+name1_lower: str = name1.lower()
+print(name1_lower)
+name1_upper: str = name1.upper()
+print(name1_upper)
+name1_title: str = name1.title()
+print(name1_title)
+
+#2-5
+print('Kobe Bryant once said: \"If you’re afraid to fail,then you’re probably going to fail.\"')
+
+#2-6
+message: str = "\"If you’re afraid to fail,then you’re probably going to fail.\""
+famous_person: str = 'Kobe Bryant'
+print(f'{famous_person} once said: {message}')
+
+#2-8
+filename: str = 'python_notes.txt'
+print(filename.removesuffix('.txt'))
+
+#3.1
+names: list = ['Valerio', 'Alessio', 'Edoardo']
+print(names[0])
+print(names[1])
+print(names[2])
+
+#3.2
+print(f'Hello {names[0]}, how are you?')
+print(f'Hello {names[1]}, how are you?')
+print(f'Hello {names[2]}, how are you?')
+
+#3-3
+mode_trasportation: list = ['motocycles', 'cars', 'airplanes']
+print(f'{mode_trasportation[2]} are really fast.')
+print(f'{mode_trasportation[1]} are cool.')
+print(f'{mode_trasportation[0]} are dangerous.')
+'''
+
+#3-4
