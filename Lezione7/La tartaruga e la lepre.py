1f29851e3dc76626d397e44ce716ea80e345726b
Gajiros <gabbi.jimros@gmail.com>
2024-05-14 12:23:28 +0200

    v2

--- Lezione7/La tartaruga e la lepre.py
+++ Lezione7/La tartaruga e la lepre.py
@@ -1,17 +1,23 @@
 import random
 
-def abilities_t(ability: int, position_t: int) -> int:
+def abilities_t(ability: int, position_t: int, weather: str) -> int:
+    
     if (1 <= ability <= 5):
         position_t += 3 
     elif (6 <= ability <= 7):
         position_t -= 6
         if (position_t < 0):
-            position_t = 1
+            position_t = 0
     else:
         position_t += 1
+    if (weather == 'pioggia'):
+        position_t -= 1
+        if (position_t < 0):
+            position_t = 0
     return position_t
 
-def abilities_h(ability: int, position_h: int):
+def abilities_h(ability: int, position_h: int, weather: str) -> int:
+    
     if (1 <= ability <= 2):
         position_h = position_h
     elif (3 <= ability <= 4):
@@ -19,30 +25,45 @@ def abilities_h(ability: int, position_h: int):
     elif (ability == 5):
         position_h -= 12
         if (position_h < 0):
-            position_h = 1
+            position_h = 0
     elif (6 <= ability <= 8):
         position_h += 1
     else:
         position_h -= 2
         if (position_h < 0):
-            position_h = 1
+            position_h = 0
+    if (weather == 'pioggia'):
+        position_h -= 2
+        if (position_h < 0):
+            position_h = 0
     return position_h
 
-lenght_route: int = 69
-route: list = ['TH'] + ['_'] * lenght_route
+def positions(route: list):
+    
+    print(route)
+
+length_route: int = 69
+route: list = ['TH'] + ['_'] * length_route
 position_t: int = 0
 position_h: int = 0
-clock: int = 0
+tick: int = 0
+weather_change: list = range(0, 99999999, 10)
+for read in weather_change:
+    print(read)
 
 print('BANG !!!!! AND THEY\'RE OFF !!!!!')
-while (clock != 999999999999):
+while (tick != 999999999999):
+    for read in weather_change:
+        if (tick == read):
+            weather: str = random.choice(['soleggiato', 'pioggia'])
+            #print(weather)
     route[position_t] = '_'
     route[position_h] = '_'
     ability_t: int = random.randint(1, 10)
     ability_h: int = random.randint(1, 10)
-    position_t = abilities_t(ability_t, position_t)
-    position_h = abilities_h(ability_h, position_h)
-    
+    position_t = abilities_t(ability_t, position_t, weather)
+    position_h = abilities_h(ability_h, position_h, weather)
+        
     if (position_t >= 69):
         print("TORTOISE WINS! || VAY!!!")
         break
@@ -52,7 +73,7 @@ while (clock != 999999999999):
     elif (position_t >= 69) and (position_h >= 69):
         print("IT'S A TIE.")
         break
-    
+
     if (route[position_t] == '_'):
         route[position_t] = 'T'
     elif (route[position_t] == 'H'):
@@ -64,6 +85,5 @@ while (clock != 999999999999):
     elif (route[position_h] == 'T'):
         print('OUCH!!!')
         route[position_h] = 'TH'
-    print(route)
-    
-    clock += 1
+    positions(route)
+    tick += 1
